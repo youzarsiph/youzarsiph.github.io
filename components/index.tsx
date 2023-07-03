c7786f17391950c60772090eab51bf84d59e00d7
Yousef Abu Shanab <josephyousef249@gmail.com>
2023-07-03 11:51:15 +0300

    Fix Can not find module... [2]

--- components/index.tsx
+++ components/index.tsx
@@ -1,7 +1,7 @@
-import Background from "./Background";
-import Badge from "./Badge";
-import Card from "./Card";
-import Container from "./Container";
-import Nav from "./Navbar";
+import Background from "./background";
+import Badge from "./badge";
+import Card from "./card";
+import Container from "./container";
+import Nav from "./navbar";
 
 export { Background, Badge, Card, Container, Nav };
